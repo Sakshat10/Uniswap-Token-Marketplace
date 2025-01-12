46d29a81594d4befae8ef71a97e73cb041138f0a
sakshat kumar <Sakshatkumar007@gmail.com>
2025-01-12 17:42:24 +0530

    feat: add contract deploy script

---
+++ deploy/scripts/deploy.js
@@ -0,0 +1,15 @@
+const hre = require("hardhat");
+
+async function main(){
+    const CustomDex = await hre.ethers.getContractFactory("CustomDex");
+    const customDex = await CustomDex.deploy();
+
+    await customDex.deployed();
+    console.log(`customDex: ${customDex.address}`)
+    
+}
+
+main().catch((error)=> {
+    console.log(error);
+    process.exitCode = 1;
+});
