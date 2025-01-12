22606b3dee7f7d31995248f78492a97bc3a7222e
sakshat kumar <Sakshatkumar007@gmail.com>
2025-01-12 17:41:50 +0530

    fix: fix hardhat config file

--- deploy/hardhat.config.js
+++ deploy/hardhat.config.js
@@ -2,15 +2,15 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
-const NEXT_PUBLIC_POLYGON_MUMBAI_RPC = "https://rpc.ankr.com/polygon_mumbai";
-const NEXT_PUBLIC_PRIVATE_KEY = "YOUR_PRIVATE_KEY";
+const NEXT_PUBLIC_POLYGON_Amoy_RPC = "https://polygon-amoy.g.alchemy.com/v2/ZaaryneY2zSqfmOsMN0oXEvLM9mvZnvC";
+const NEXT_PUBLIC_PRIVATE_KEY = "07f39952cd2fb010c58a9163261b7fcf654ce6e9b6234269d1087ac2c96b21a2";
 module.exports = {
   solidity: "0.8.0",
-  defaultNetwork: "matic",
+  defaultNetwork: "amoy",
   networks: {
     hardhat: {},
-    polygon_mumbai: {
-      url: NEXT_PUBLIC_POLYGON_MUMBAI_RPC,
+    polygon_amoy: {
+      url: NEXT_PUBLIC_POLYGON_Amoy_RPC,
       accounts: [`0x${NEXT_PUBLIC_PRIVATE_KEY}`],
     },
   },
